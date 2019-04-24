66a1e52d439cb24ccc01fa554b153e0b9e74c243
MaritVD <49484006+MaritVD@users.noreply.github.com>
2019-04-24 20:08:28 +0200

    Add files via upload
    Ik krijg de volledige mapjes niet geupload.. Maar dit zijn alle bestanden.

---
+++ polls/urls.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 18 16:19:36 2019
+
+@author: M. van Dijk
+"""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DataView.as_view(), name='data'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
